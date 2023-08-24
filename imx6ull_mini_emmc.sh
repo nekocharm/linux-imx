c8da14fc5eaad550028410ad11e7fa2bdda00df2
nekocharm <zhangchi.mail@qq.com>
2023-08-24 11:01:39 +0800

    add mini board

---
+++ imx6ull_mini_emmc.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_mini_emmc_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j2
